202ddbb058b14b54314bbaa7e2ee6439f601697d
lopesdaluz <tlopesdaluz@gmail.com>
2024-11-13 21:16:08 +0100

    fixed a warning from my console

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -57,7 +57,7 @@ function CreatePost() {
           <label>Title: </label>
           <ErrorMessage name="title" component="span" />
           <Field
-            autocomplete="off"
+            autoComplete="off"
             id="inputCreatePost"
             name="title"
             placeholder="(Ex. Title...)"
@@ -65,7 +65,7 @@ function CreatePost() {
           <label>Post: </label>
           <ErrorMessage name="postText" component="span" />
           <Field
-            autocomplete="off"
+            autoComplete="off"
             id="inputCreatePost"
             name="postText"
             placeholder="(Ex. Post...)"
